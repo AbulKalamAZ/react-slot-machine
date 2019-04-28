6ddfae80c4f8c736cc46fad5fe8a71382620cd52
AbulKalamAZ <dev.akazad@hotmail.com>
2019-04-28 22:19:35 +0600

    Updates

---
+++ precache-manifest.2cefe8296d1fcae8b315d1390eba270e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf595663af17254540f01f57c9d31a09",
+    "url": "/react-slot-machine/static/media/texturetastic_gray.bf595663.png"
+  },
+  {
+    "revision": "b720985de975dfd3160b",
+    "url": "/react-slot-machine/static/js/runtime~main.b720985d.js"
+  },
+  {
+    "revision": "8c0d6876c708ff83ce1c",
+    "url": "/react-slot-machine/static/js/main.8c0d6876.chunk.js"
+  },
+  {
+    "revision": "210479ea1b271a570445",
+    "url": "/react-slot-machine/static/js/1.210479ea.chunk.js"
+  },
+  {
+    "revision": "8c0d6876c708ff83ce1c",
+    "url": "/react-slot-machine/static/css/main.6047c2d1.chunk.css"
+  },
+  {
+    "revision": "733101c2380327f7166ce5520d13bab1",
+    "url": "/react-slot-machine/index.html"
+  }
+];
